af485220fbf96ee8c99b7c923552071b8afaee54
reign001 <olochej543@gmail.com>
2024-03-12 14:40:14 -0700

    fabric

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+!#/usr/bin/env python3
+"""a Bash script that sets up your web servers for the deployment of web_static"""
+from fabric.api import *
+from datetime import datetime
+import os
+def do_pack():
+
+	local("sudo mkdir -p versions")
+	t = datetime.now()
+	t_str = t.strftime('%Y%m%d%H%M%S')
+
+	local(f'sudo tar -cvzf versions/web_static_{t_str}.tgz web_static')
+
+	f_path = f"versions/web_static_{t_str}.tgz"
+	f_size = os.path.getsize(f_path)
+	print(f'web_static packed: {f_path} -> {f_size)Bytes')
+
